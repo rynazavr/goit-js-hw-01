946da738564cac142a419ea504097a84338b4f92
rynazavr <lunevaira@gmail.com>
2020-05-12 18:18:39 +0300

    tasks 1-6

--- module1/task-6.js
+++ module1/task-6.js
@@ -1,1 +1,16 @@
 `use strict`;
+let total = 0;
+let input;
+do {
+  input = prompt("Введите число!");
+  if (input === null) {
+    break;
+  }
+  if (Number.isNaN(Number(input))) {
+    alert("Попробуйте ввести число еще раз!");
+  } else {
+    total = total + Number(input);
+  }
+} while (input !== null);
+
+alert(`Общая сумма: ${total}.`);
