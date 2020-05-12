08d5588710f4be8d6ddf9c76222ac45bd781fdac
rynazavr <lunevaira@gmail.com>
2020-05-12 17:09:57 +0300

    1-5 tasks done

--- module1/task-1.js
+++ module1/task-1.js
@@ -1,6 +1,8 @@
 `use strict`;
 const name = `Генератор защитного поля`;
 let price = 1000;
-const message = `Выбран ${name}, цена за штуку ${price} кредитов`;
+let message = `Выбран ${name}, цена за штуку ${price} кредитов`;
 console.log(message);
 price = 2000;
+message = `Выбран ${name}, цена за штуку ${price} кредитов`;
+console.log(message);
--- module1/task-5.js
+++ module1/task-5.js
@@ -1,1 +1,26 @@
 `use strict`;
+let deliveryToCountry = prompt(
+  `Выбирите страну доставки товара: Китай, Чили, Австралия, Индия или Ямайка`
+);
+let country = deliveryToCountry.toLowerCase();
+let message;
+switch (country) {
+  case `китай`:
+    alert(`100 кредитов`);
+    break;
+  case `чили`:
+    alert(`250 кредитов`);
+    break;
+  case `австралия`:
+    alert(`170 кредитов`);
+    break;
+  case `индия`:
+    alert(`80 кредитов`);
+    break;
+  case `ямайка`:
+    alert(`120 кредитов`);
+    break;
+  default:
+    alert(`Неверное значение`);
+}
+console.log(message);
