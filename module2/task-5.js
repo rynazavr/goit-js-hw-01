aaff9a58cef007045df89c80631c57241e7f5cc6
rynazavr <lunevaira@gmail.com>
2020-05-14 22:27:29 +0300

    module2 task_5 without false

--- module2/task-5.js
+++ module2/task-5.js
@@ -1,11 +1,13 @@
 `use strict`;
 const checkForSpam = function (message) {
-  // твой код
+  const messageLowerCased = message.toLowerCase();
+  console.log(messageLowerCased);
+  if (messageLowerCased.includes("spam" || "sale")) {
+    return true;
+  }
+  return false;
 };
 
-/*
- * Вызовы функции для проверки работоспособности твоей реализации.
- */
 console.log(checkForSpam("Latest technology news")); // false
 
 console.log(checkForSpam("JavaScript weekly newsletter")); // false
@@ -17,4 +19,4 @@ console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
 /* Напиши функцию checkForSpam(message), принимающую 1 параметр message - строку. 
 Функция проверяет ее на содержание слов spam и sale. Если нашли зарещенное слово 
 то функция возвращает true, если запрещенных слов нет функция возвращает false. 
-Слова в строке могут быть в произвольном регистре.
+Слова в строке могут быть в произвольном регистре.*/
