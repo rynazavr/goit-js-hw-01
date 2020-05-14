056a1a0463c1c7954324150a46050823ec1a9c16
rynazavr <lunevaira@gmail.com>
2020-05-14 19:58:08 +0300

    module2 task3

--- module2/task-3.js
+++ module2/task-3.js
@@ -1,6 +1,13 @@
 `use strict`;
 const findLongestWord = function (string) {
-  // твой код
+  const splitWordsInString = string.split(" ");
+  let longestWordInString = "";
+  for (let i = 0; i < splitWordsInString.length; i += 1) {
+    if (splitWordsInString[i].length > longestWordInString.length) {
+      longestWordInString = splitWordsInString[i];
+    }
+  }
+  return longestWordInString;
 };
 
 /*
@@ -13,4 +20,4 @@ console.log(findLongestWord("Google do a roll")); // 'Google'
 console.log(findLongestWord("May the force be with you")); // 'force'
 
 /*Напиши фукцнию findLongestWord(string), которая принимает параметром произвольную строку 
-(в строке будут только слова и пробелы) и возвращает самое длинное слово в этой строке.
+(в строке будут только слова и пробелы) и возвращает самое длинное слово в этой строке.*/
