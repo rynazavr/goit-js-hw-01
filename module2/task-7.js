cb8bb8fe55d637a5678bd326712bb0925ea3861f
rynazavr <lunevaira@gmail.com>
2020-05-15 15:46:51 +0300

    module2 task6

--- module2/task-7.js
+++ module2/task-7.js
@@ -1,3 +1,4 @@
+/*НЕ ОБЯЗАТЕЛЬНЫЙ ТАСК - можно сделть позже, по желанию*/
 `use strict`;
 const logins = ["Mango", "robotGoogles", "Poly", "Aj4x1sBozz", "qwerty123"];
 
@@ -21,5 +22,4 @@ console.log(addLogin(logins, "robotGoogles")); // 'Такой логин уже 
 console.log(addLogin(logins, "Zod")); // 'Ошибка! Логин должен быть от 4 до 16 символов'
 console.log(addLogin(logins, "jqueryisextremelyfast")); // 'Ошибка! Логин должен быть от 4 до 16 символов'
 
-
-/* https://github.com/goitacademy/javascript-homework/tree/master/homework-02
+/* https://github.com/goitacademy/javascript-homework/tree/master/homework-02*/
