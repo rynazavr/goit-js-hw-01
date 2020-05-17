e2d8d85665bbdb360aeb911b91d1a18c5cae43de
rynazavr <lunevaira@gmail.com>
2020-05-17 18:30:56 +0300

    task5 done

--- module3/task-4.js
+++ module3/task-4.js
@@ -1,1 +1,28 @@
 `use strict`;
+const countTotalSalary = function (employees) {
+  // твой код
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(countTotalSalary({})); // 0
+
+console.log(
+  countTotalSalary({
+    mango: 100,
+    poly: 150,
+    alfred: 80,
+  })
+); // 330
+
+console.log(
+  countTotalSalary({
+    kiwi: 200,
+    lux: 50,
+    chelsy: 150,
+  })
+); // 400
+// Напиши функцию countTotalSalary(employees) принимающую объект зарплат.
+// Функция считает общую сумму запрплаты работников и возращает ее.Каждое поле объекта,
+//     передаваемого в функцию, имеет вид "имя": "зарплата".
--- module3/task-5.js
+++ module3/task-5.js
@@ -1,1 +1,31 @@
 `use strict`;
+const products = [
+  { name: "Радар", price: 1300, quantity: 4 },
+  { name: "Сканер", price: 2700, quantity: 3 },
+  { name: "Дроид", price: 400, quantity: 7 },
+  { name: "Захват", price: 1200, quantity: 2 },
+];
+
+const getAllPropValues = function (arr, prop) {
+  const values = [];
+  for (const ar of arr) {
+    // console.log(ar[prop]);
+    if (prop in ar) {
+      values.push(ar[prop]);
+    }
+  }
+  return values;
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(getAllPropValues(products, "name")); // ['Радар', 'Сканер', 'Дроид', 'Захват']
+
+console.log(getAllPropValues(products, "quantity")); // [4, 3, 7, 2]
+
+console.log(getAllPropValues(products, "category")); // []
+
+// Напиши функцию getAllPropValues(arr, prop), которая получает массив
+// объектов и имя свойства.Возвращает массив
+// значений определенного свойства prop из каждого объекта в массиве.
