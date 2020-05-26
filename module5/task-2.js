ceadc57c0e24473059cec20aec84d03e96b8b4fa
rynazavr <lunevaira@gmail.com>
2020-05-26 13:48:58 +0300

    module5 task2

--- module5/task-2.js
+++ module5/task-2.js
@@ -1,15 +1,29 @@
 `use strict`;
-const User = function ({ name: name, age: age, followers: followers }) {
-  this.name = name;
-  this.age = age;
-  this.followers = followers;
-};
 
-User.prototype.getInfo = function () {
-  console.log(
-    `User ${this.name} is ${this.age} years old and has ${this.followers} followers`
-  );
-};
+class User {
+  constructor({ name, age, followers }) {
+    this.name = name;
+    this.age = age;
+    this.followers = followers;
+  }
+  getInfo() {
+    console.log(
+      `User ${this.name} is ${this.age} years old and has ${this.followers} followers`
+    );
+  }
+}
+
+// const User = function ({ name, age, followers }) {
+//   this.name = name;
+//   this.age = age;
+//   this.followers = followers;
+// };
+
+// User.prototype.getInfo = function () {
+//   console.log(
+//     `User ${this.name} is ${this.age} years old and has ${this.followers} followers`
+//   );
+// };
 
 const mango = new User({
   name: "Mango",
