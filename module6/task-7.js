a6d0ee7e32af1f879cac8b3737eccc581a4bef68
rynazavr <lunevaira@gmail.com>
2020-05-28 17:53:23 +0300

    task 7 done

--- module6/task-7.js
+++ module6/task-7.js
@@ -1,7 +1,6 @@
 //Получить общую сумму баланса (поле balance) всех пользователей.
 import users from "./users.js";
-const calculateTotalBalance = (users) => {
-  // твой код
-};
+const calculateTotalBalance = (users) => users.reduce((acc, users) =>
+  acc + users.balance, 0)
 
 console.log(calculateTotalBalance(users)); // 20916
