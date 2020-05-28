02243d9d99e162cac3d55563e0707dd8e49d376a
rynazavr <lunevaira@gmail.com>
2020-05-28 18:57:53 +0300

    9done

--- module6/task-9.js
+++ module6/task-9.js
@@ -1,8 +1,7 @@
 import users from "./users.js";
 //Массив имен (поле name) людей, отсортированных в зависимости от количества их друзей (поле friends)
-const getNamesSortedByFriendsCount = (users) => {
-  // твой код
-};
+const getNamesSortedByFriendsCount = (users) => users.sort((a, b) => a.friends.length - b.friends.length).map(user => user.name)
 
 console.log(getNamesSortedByFriendsCount(users));
 // [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
+
