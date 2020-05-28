6bfd2c77fba7b8fa244457e32bf4b77eba31b9ac
rynazavr <lunevaira@gmail.com>
2020-05-28 17:35:41 +0300

    5task done

--- module6/task-5.js
+++ module6/task-5.js
@@ -1,8 +1,6 @@
 //Получить пользоваля (не массив) по email (поле email, он уникальный).
 import users from "./users.js";
-const getUserWithEmail = (users, email) => {
-  // твой код
-};
+const getUserWithEmail = (users, email) => users.filter(user => user.email.includes(email))
 
 console.log(getUserWithEmail(users, "shereeanthony@kog.com")); // {объект пользователя Sheree Anthony}
 console.log(getUserWithEmail(users, "elmahead@omatom.com")); // {объект пользователя Elma Head}
