1250bdce35898b9415e0ff8078695ebbd590a718
rynazavr <lunevaira@gmail.com>
2020-05-28 19:51:40 +0300

    10 undone

--- module6/task-10.js
+++ module6/task-10.js
@@ -1,8 +1,14 @@
 import users from "./users.js";
 //Получить массив всех умений всех пользователей (поле skills), при этом не должно быть повторяющихся умений и они должны быть отсортированы в алфавитном порядке.
-const getSortedUniqueSkills = (users) => {
-  // твой код
-};
+const getSortedUniqueSkills = (users) => users.reduce((allSkills, item) => {
+  allSkills.push(...item.skills);
+  return allSkills;
+}, [])
+
+// .sort((a, b) => a.skills - b.skills)
+
+
 
 console.log(getSortedUniqueSkills(users));
 // [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam' ]
+
