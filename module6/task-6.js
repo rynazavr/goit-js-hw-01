62b9a71fd70ebcbf8e06d92c4de482cad65f03d9
rynazavr <lunevaira@gmail.com>
2020-05-28 17:41:58 +0300

    6 done

--- module6/task-6.js
+++ module6/task-6.js
@@ -1,8 +1,6 @@
 //Получить массив пользователей попадающих в возрастную категорию от min до max лет (поле age).
 import users from "./users.js";
-const getUsersWithAge = (users, min, max) => {
-  // твой код
-};
+const getUsersWithAge = (users, min, max) => users.filter(user => user.age < max && user.age > min)
 
 console.log(getUsersWithAge(users, 20, 30)); // [объект Ross Vazquez, объект Elma Head, объект Carey Barr]
 
