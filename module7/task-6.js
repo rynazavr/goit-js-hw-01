1a3011b5d4dcd9f09424d2b8b9ca76e90e2ecece
rynazavr <lunevaira@gmail.com>
2020-06-02 17:31:10 +0300

    6 done

--- module7/task-6.js
+++ module7/task-6.js
@@ -4,3 +4,18 @@
 Если введено подходящее количество, то border инпута становится зеленым, если 
 неправильное - красным.
 Для добавления стилей, используй CSS-классы valid и invalid.*/
+const inputSix = document.querySelector("#validation-input");
+const numberMax = inputSix.getAttribute("data-length");
+// console.log(numberMax);
+// console.dir(inputSix);
+//блок слушателя событий
+inputSix.addEventListener("blur", handleInput);
+//блок событий
+
+function handleInput(event) {
+  if (inputSix.value.length < numberMax) {
+    inputSix.classList.add("valid"), inputSix.classList.remove("invalid");
+  } else if (inputSix.value.length > numberMax) {
+    inputSix.classList.add("invalid"), inputSix.classList.remove("valid");
+  }
+}
