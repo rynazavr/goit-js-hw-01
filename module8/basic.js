60ec065c25ea31620873aa5dad5e405a81078abb
rynazavr <lunevaira@gmail.com>
2020-06-03 22:24:52 +0300

    big photo

--- module8/basic.js
+++ module8/basic.js
@@ -1,41 +1,26 @@
 import imagelist from "./gallery-items.js";
 import { templeteImageCard } from "./gallery-skeleton.js";
-// console.log(templeteImageCard);
+
 const galleryUl = document.querySelector(`.js-gallery`);
 const buttonClose = document.querySelector(`button[data-action="close-modal"]`);
 const divModal = document.querySelector(`.js-lightbox`);
+const imgModal = document.querySelector(`.lightbox__image`);
 
 //image block
 const allLi = imagelist
   .map((imageItem) => templeteImageCard(imageItem))
   .join(``);
 galleryUl.insertAdjacentHTML(`beforeend`, allLi);
-// console.log(galleryUl);
 
-// galleryUl.addEventListener(`click`, clickList);
-// function clickList(event) {
-//   //   console.log(event);
-//   console.log(event.target);
-//   if (event.target.nodeName !== "IMG") return;
-//   //   const source = event.target.dataset.source;
-//   const { source } = event.target.dataset;
-//   //   console.log(source);
-//   event.target.src = source;
-// }
 galleryUl.addEventListener(`click`, clickList);
 buttonClose.addEventListener(`click`, clickButtonClosed);
 
 function clickList(event) {
   window.addEventListener(`keydown`, onEscape);
   //принимает onEscape функцию - закрыти модалки по ESC
-  divModal.classList.add("is-open");
-  console.log(event.target);
   if (event.target.nodeName !== "IMG") return;
-  console.log(divModal);
-  //   document.body.classList.add("lightbox");
-  //   const { source } = event.target.dataset;
-  //   console.log(source);
-  //   event.target.src = source;
+  divModal.classList.add("is-open");
+  imgModal.src = event.target.dataset.source;
 }
 
 function clickButtonClosed(event) {
